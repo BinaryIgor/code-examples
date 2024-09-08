d106965fcbeaf78ff24bfe8683828d606c81cd55
Igor <41533396+BinaryIgor@users.noreply.github.com>
2024-09-08 18:28:56 +0200

    Refinements

--- vector-embeddings-with-postgres/generate_google_vector_embeddings.bash
+++ vector-embeddings-with-postgres/generate_google_vector_embeddings.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-skip="${SKIP:-0}"
-curl -X POST "http://localhost:8080/vector-embeddings/generate?model=GOOGLE_TEXT_004&dataType=Books&skip=${skip}&batchSize=50&rateLimitDelay=1000"
+skip="${VEMBEDDINGS_SKIP:-0}"
+curl -X POST "http://localhost:8080/vector-embeddings/generate?model=GOOGLE_TEXT_004&dataType=Books&skip=${skip}&batchSize=50&rateLimitDelay=500"
