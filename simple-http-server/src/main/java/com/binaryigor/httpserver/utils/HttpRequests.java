8e9ab4f0d2a6cb28ca2e5a0c041d2d418a03d327
Igor <igor@binaryigor.com>
2025-04-19 08:36:16 +0200

    Simple HTTP server in Java: HTML manipulation start

--- simple-http-server/src/main/java/com/binaryigor/httpserver/utils/HttpRequests.java
+++ simple-http-server/src/main/java/com/binaryigor/httpserver/utils/HttpRequests.java
@@ -1,6 +1,5 @@
 package com.binaryigor.httpserver.utils;
 
-import java.net.URLDecoder;
 import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,26 +7,12 @@ import java.util.Map;
 public class HttpRequests {
 
     public static Map<String, String> parseUrlEncodedForm(byte[] rawForm, Charset charset) {
-        var keyValues = URLDecoder.decode(new String(rawForm, charset), charset).split("&");
         var form = new HashMap<String, String>();
-        for (var kv : keyValues) {
-            var keyValue = kv.split("=", 2);
-            form.put(keyValue[0].strip(), keyValue.length == 1 ? "" : keyValue[1].strip());
-        }
         return form;
     }
 
     public static Map<String, String> parseQueryParams(String url, Charset charset) {
-        System.out.println("Url to decode: " + url);
-        var startIdx = url.indexOf("?");
-        var keyValues = URLDecoder.decode(startIdx >= 0 ? url.substring(startIdx + 1) : url, charset).split("&");
         var params = new HashMap<String, String>();
-        for (var kv : keyValues) {
-            var keyValue = kv.split("=", 2);
-            var key = keyValue[0].strip();
-            var value = keyValue.length == 1 ? "" : keyValue[1].strip();
-            params.merge(key, value, (a, b) -> a + "," + b);
-        }
         return params;
     }
 }
