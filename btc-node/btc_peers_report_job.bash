ea8d767ccab37486c2bd841f1b439b07a3be3d84
Igor <igor@binaryigor.com>
2025-04-21 07:11:00 +0200

    Improve btc_peers_report_job script

--- btc-node/btc_peers_report_job.bash
+++ btc-node/btc_peers_report_job.bash
@@ -3,11 +3,12 @@ set -eu
 
 report_name="peers_$(date +"%Y%m%d%H%M").txt"
 reports_dir=${REPORTS_DIR:-"/home/bigor/btc-peers"}
-report_path="${reports_dir}/${report_name}"
+report_dir="${reports_dir}/$(date +"%Y%m")"
+report_path="${report_dir}/${report_name}"
 btc_explorer_path=${BTC_EXPLORER_PATH:-"/home/bigor/scripts/btc_explorer.py"}
 
 echo "Creating peers report and saving it under ${report_path} path"
-mkdir -p $reports_dir
+mkdir -p $report_dir
 
 export BITCOIN_CLI_CMD="/home/bigor/bitcoin-27.0/bin/bitcoin-cli"
 
